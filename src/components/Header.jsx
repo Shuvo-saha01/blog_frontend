6c33e6d07e5ef46b3718714f98be571eb846b509
muskaand26 <muskaandas2004@gmail.com>
2024-12-05 22:34:07 +0530

    Updated ESLint config, fixed package issues, removed old components, and added new PostCSS config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 text-white p-4">
+    <header className="bg-blue-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold">My Blog</h1>
         <nav>
