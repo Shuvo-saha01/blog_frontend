a16d800230f464fa19238cf7b70d03f54f26e2f5
Shuvo-saha01 <onlyshuvosaha11@gmail.com>
2024-12-14 21:18:59 +0530

    Merge changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //router setup
 import App from './App.jsx'
 import React from 'react';
-import './index.css';  // Or the path to your main CSS file
+import './index.css'; 
 import Login from './pages/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
