5a98532cba28a6bfdf4103910092711e03678175
sagario9 <saikatsardar180@gmail.com>
2024-12-18 22:53:11 +0530

    Added Functional Navbar and other Functions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,15 @@ const Header = () => {
 
   const handleLoginClick = () => {
     navigate('/login');
+  }
+  const aboutSection = () => {
+    navigate('/about');
+  }
+  const home = () => {
+    navigate('/');
+  }
+  const Contact = () => {
+    navigate('/contact');
   };
 
   return (
@@ -18,9 +27,9 @@ const Header = () => {
         <h1 className="text-2xl font-bold">My Blog</h1>
         <nav>
           <ul className="flex space-x-4">
-            <li className='py-2'><a href="#home" className="hover:underline">Home</a></li>
-            <li className='py-2'><a href="#about" className="hover:underline">About</a></li>
-            <li className='py-2'><a href="#contact" className="hover:underline">Contact</a></li>
+            <li className='py-2'><button onClick={home} className="hover:underline">Home</button></li>
+            <li className='py-2'><button onClick={aboutSection} className="hover:underline">About</button></li>
+            <li className='py-2'><button onClick={Contact} className="hover:underline">Contact</button></li>
           <button onClick={handleLoginClick} className="login hover:border hover:border-white px-5 py-2 bg-blue-900 rounded-[3rem]">Log In</button>
           </ul>
         </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; //rou
 import App from './App.jsx'
 import React from 'react';
 import './index.css';  // Or the path to your main CSS file
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';
 import Login from './pages/Login.jsx';
 
 createRoot(document.getElementById('root')).render(
@@ -13,6 +15,8 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         {/* route for login page */}
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path="/login" element={<Login />} /> 
       </Routes>
     </Router>
